8685d41c5f5534b021fa21bea058e6a1fea9a275
ESAISS <117594997+ESAISS@users.noreply.github.com>
2022-12-30 22:10:36 +0100

    Update easytest.js

--- javascript/easytest.js
+++ javascript/easytest.js
@@ -1,7 +1,6 @@
 function easyverif(nbCheck){
    if(document.getElementById(nbCheck).checked == true){       
     alert("Good job !");
-        document.getElementById('easyverif').submit();
     }
     else{
         alert("You are mistaken.");
