593d70ddd9bb38ab04e1bf7b41fd6eea6393d6ed
ESAISS <117594997+ESAISS@users.noreply.github.com>
2023-01-02 14:42:18 +0100

    Update easytest.js

--- javascript/easytest.js
+++ javascript/easytest.js
@@ -9,7 +9,7 @@ function easyverif(nbCheck){
 }
 function in1check(){
 var enter1 =document.getElementById("jesuis").value;
-   if enter1 == "I am" {
+   if (enter1 == "I am") {
    alert("Good job !");
    }
     else{
@@ -19,7 +19,7 @@ var enter1 =document.getElementById("jesuis").value;
 
 function in2check(){
 var enter2 =document.getElementById("have").value;
-    if enter2 == "am" {
+    if (enter2 == "am") {
    alert("Good job !");
    }
     else{
